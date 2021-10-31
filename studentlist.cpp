cbfd8c858e54185aa4d84a61b3f90e66e78983da
ehanm <452735@bsd48.org>
2021-10-31 20:01:30 +0100

    errors, need help

--- studentlist.cpp
+++ studentlist.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+
 struct Student {
   char firstname[10];
   char lastname[10];
@@ -11,27 +12,82 @@ struct Student {
   float gpa;
 };
 
+vector<Student> studentlist;
+
+void add(){
+
+  Student student;
+
+  cout << "Insert the student's first name" << endl;
+  cin >> student.firstname;
+
+  cout << "Insert the student's last name" << endl;
+  cin >> student.lastname;
+
+  cout << "Insert the student's id" << endl;
+  cin >> student.id;
+
+  cout << "Insert the student's gpa" << endl;
+  cin >> student.gpa;
+
+  studentlist.push_back(student);
+  
+}
+
+
+
+void print() {
+
+  Student student;
+
+  cout << firstname
+  
+}
+
+
+
 int main() {
 
   Student student;
-  char input[100];
-  char list[100];
   bool cont = true;
+  char input[100];
+
+  cout << "Welcome to Student List! This program will read in students, information about the students, print them out, and delete them.";
   
   while (cont == true) {
+    
     cout << "What would you like to do? (ADD, PRINT, DELETE, QUIT)" << endl;
 
     cin >> input;
 
     if (strcmp(input, "ADD") == 0) {
 
-      cout << "Enter the student's first name" << endl;
-      cin >> student.firstname;
-      cout << student.firstname << endl;
+      add(&student);
+     
+    }
+    if else (strcmp(input, "DELETE") == 0) {
+
+      cout << "Delete thing";
       
+    }
+    if else (strcmp(input, "PRINT") == 0) {
+
+      cout << "Print thing";
+    }
+    if else (strcmp(input, "QUIT") == 0) {
+
+      cont = false;
+      break;
+      
+    }
+    else {
+
+      cout << "Invalid input!" << endl;
     }
 
     
   }
+
+  return 0;
   
 }
