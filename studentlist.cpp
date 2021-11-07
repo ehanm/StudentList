7b323db521db0360a75a375b9716aad716f33e51
ehanm <452735@bsd48.org>
2021-11-07 05:51:37 +0000

    final push with comments

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,3 +1,7 @@
+// This is a program that creates a list of students (gets their name, id, and gpa) and is able to add, delete, and print out the list! The commands are "ADD", "PRINT", "DELETE", and "QUIT"
+// Made by Ehan Masud
+// Completed on 11/6/2021
+
 #include <iostream>
 #include <cstring>
 #include <vector>
@@ -5,16 +9,16 @@
 
 using namespace std;
 
-struct Student {
+struct Student { // struct to add for each person
   char firstname[200];
   char lastname[200];
   int id;
   float gpa;
 };
 
-vector<Student*> studentlist;
+vector<Student*> studentlist; // making a vector
 
-void add(vector<Student*> &s){
+void add(vector<Student*> &s){ // function to add students
   
   Student* student = new Student();
   
@@ -30,26 +34,24 @@ void add(vector<Student*> &s){
   cout << "Insert the student's gpa" << endl;
   cin >> student->gpa;
 
-  (s).push_back(student);
+  (s).push_back(student); // sends information to vector
   
 }
 
-
-
-void print(vector<Student*> &s) {
+void print(vector<Student*> &s) { // function to print all students
 
   vector<Student*>::iterator it;
 
-  for (it = (s).begin(); it < (s).end(); it++) {
+  for (it = (s).begin(); it < (s).end(); it++) { // iterates through the vector and sends out information 
     cout << (*it)->firstname << " ";
     cout << (*it)->lastname << " ";
     cout << (*it)->id << " ";
-    cout << fixed << setprecision(2) << (*it)->gpa << "\n"; 
+    cout << fixed << setprecision(2) << (*it)->gpa << "\n";  //after information of a student is done, start printing from another line
   }
   
 }
 
-void deletestudent(vector<Student*> &s) {
+void deletestudent(vector<Student*> &s) { // function to delete students
   
   int idnumber;
   vector<Student*>::iterator it;
@@ -58,7 +60,7 @@ void deletestudent(vector<Student*> &s) {
   cout << "Insert the ID number of the student: " << endl;
   cin >> idnumber;
 
-  for (it = s.begin(); it < s.end(); it++) {
+  for (it = s.begin(); it < s.end(); it++) { // iterates through vector and if at a point in the vector the specific id is right, use erase()
     if ((*it)->id == idnumber) {
       s.erase(it);
       return;
@@ -77,13 +79,13 @@ int main() {
   bool cont = true;
   char input[100];
 
-  cout << "Welcome to Student List! This program will read in students, information about the students, print them out, and delete them.";
+  cout << "Welcome to Student List! This program will read in students, information about the students, print them out, and delete them." << endl;
   
   while (cont == true) {
     
     cout << "What would you like to do? (ADD, PRINT, DELETE, QUIT)" << endl;
 
-    cin >> input;
+    cin >> input; // inputs and depending on input, 
 
     if (strcmp(input, "ADD") == 0) {
 
@@ -102,13 +104,15 @@ int main() {
     else if (strcmp(input, "QUIT") == 0) {
 
       cont = false;
-      break;
+      break; // end the program
       
     }
     else {
 
       cout << "Invalid input!" << endl;
     }
+
+    memset(input, 0, 100); // clear the input after each input (kept getting random "invalid inputs")
 
     
   }
